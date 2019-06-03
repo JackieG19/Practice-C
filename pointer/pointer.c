e344f698d8374032ca779acbd4faaa02d94b1c75
Jackie <JGtheophila19@gmail.com>
2019-06-02 21:54:33 -0400

    using a pointer to address another integer

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -1,26 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// int add is a function with 2 arguments()
+// (integer *x is a pointer *, integer of y)
+// x = a , y = b
+
 int add(int *x, int y)
 {
-    *x += 3;
+    *x += 3; // what x is address to + 3
     return *x + y;
 }
 
 int main(void)
 {
-    printf("hello\n");
+    // printf("hello\n");
     int a = 5;
     int b = 10;
 
-    printf("the total is: %i\n", add(&a, b));
+    // this print triggers the int add function
     printf("the total is: %i\n", add(&a, b));
 
+    // once the int add function is done, it removes itself...
 
-    // int* intPointer;
-    // intPointer = &b;
-    // printf("b is: %i\n", b);
-    // *intPointer = 47;
-    // printf("b is: %i\n", b);
+    printf("the total is: %i\n", add(&a, b));
+    // and recreates again triggered by the second print);
 
 }
+
+/*
+in the terminal
+
+the total is: 18
+the total is: 21
+
+*/
