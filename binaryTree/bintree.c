a5791a09fb9d33a1ae487b9f8cf19b468887a882
Jackie <JGtheophila19@gmail.com>
2019-06-22 23:31:53 -0400

    removing nodes going thru the tree

--- binaryTree/bintree.c
+++ binaryTree/bintree.c
@@ -82,10 +82,52 @@ void displayDeparture(NODE *curNode)
 
 int doesContain(int value)
 {
-    //search the tree
+    // search the tree
     // return 0 if value is not in the tree
-    // return 1 if value does not exist in the tree
+    // return 1 if value does exist in the tree
+
+    // if (rootNode->value == value)
+    // {
+    //     return 1;
+    // }
+    NODE *trav = rootNode;
+    while(1)
+    {
+        if (trav->value == value)
+        {
+            return 1;
+        }
+        if (value < trav->value)
+        {
+            if (trav->low == NULL)
+            {
+                return 0;
+            }
+            trav = trav->low;
+            continue;
+        }
+        if (!trav->high)
+        {
+            return 0;
+        }
+        trav = trav->high;
+        continue;
+    }
 }
+
+void tests()
+{
+    assert(doesContain(10) && "tree does contain 10");
+    assert(doesContain(8) && "tree does contain 8");
+    assert(doesContain(4) && "tree does contain 4");
+    assert(!doesContain(1) && "tree does not contain 1");
+    assert(doesContain(15) && "tree does contain 15");
+    assert(!doesContain(100) && "Tree does not contain 100");
+    assert(doesContain(13) && "tree does contain 13");
+    assert(doesContain(7) && "tree does contain 7");
+    assert(!doesContain(16) && "tree does not contain 16");
+}
+
 
 int main(void)
 {
@@ -103,4 +145,8 @@ int main(void)
     addNode(7);
 
     displayTree(rootNode);
+    tests();
+    printf("\n");
+
+    // printf("%i\t%i\n", rootNode->low->value, rootNode->high->value);
 }
